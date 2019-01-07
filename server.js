71f140c6f748b0198416a06a57306c097c2385fe
Glitch (lava-spy) <none>
2019-01-07 09:17:09 +0000

    🚣😜 Checkpoint ./server.js:2689143/357

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 var cors = require('cors');
 
 var app = express();
@@ -13,13 +13,20 @@ var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
 // mongoose.connect(process.env.MONGOLAB_URI);
-
+app.use(bodyParser.json());
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 
 app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/new/:urlToShorten(*)', 
+        (req, res, next) => {
+          let { urlToShorten } = req.params;
+          console.log(urlToShorten);
+
+});
 
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
